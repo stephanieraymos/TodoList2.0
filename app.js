b8ae8842fb989ab6b9fd2aa11dd1182e27a9e03b
stephanieraymos <stephanieraymos@gmail.com>
2020-08-05 18:01:57 -0700

    Event listener + addTodo Function

--- app.js
+++ app.js
@@ -2,6 +2,21 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+
 //Event Listeners
+todoButton.addEventListener('click', addTodo);
+
 
 //Functions
+
+function addTodo(event){
+    // Prevent form from submitting
+    event.preventDefault();
+    //Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Create LI
+    const newTodo =document.createElement('li');
+    newTodo.innerText = "hey";
+    newTodo.classList.add("todo-item");
+}
