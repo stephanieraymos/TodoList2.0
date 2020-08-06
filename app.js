e8ab135404cc6a21d1bdcedaf3671ea4495a5dd5
stephanieraymos <stephanieraymos@gmail.com>
2020-08-05 22:55:12 -0700

    Changed value of newTodo to todoInput: it now reflects the inputted todo when added

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function addTodo(event) {
     todoDiv.classList.add("todo");
     //Create LI
     const newTodo = document.createElement('li');
-    newTodo.innerText = "hey";
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
     //Completed button
