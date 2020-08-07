4342e9ff6568bdf3cad98c5ad54bd23017db3f01
stephanieraymos <stephanieraymos@gmail.com>
2020-08-07 13:16:11 -0700

    Added event listener after animation to actually delete the item

--- app.js
+++ app.js
@@ -43,7 +43,11 @@ function deleteCheck(e) {
     //Delete todo
     if (item.classList[0] === 'trash-btn') {
         const todo = item.parentElement;
+        //Animation
         todo.classList.add('fall');
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        });
     }
 
     //Checkmark
